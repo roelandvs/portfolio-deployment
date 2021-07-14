5d25ef95d1c9911cafc1535b86c404df6c65e4d4
Roeland <roelandsteevan@gmail.com>
2021-07-14 15:39:34 +0200

    remove progressive enhancement

--- src/js/nav-dropdown.js
+++ src/js/nav-dropdown.js
@@ -3,15 +3,7 @@ const navIcon = document.querySelector('#nav-icon');
 const navLinks = document.querySelector('#main-nav ul');
 const root = document.documentElement;
 
-root.style.setProperty('--nav-pos', 'sticky');
-root.style.setProperty('--ul-pos', 'absolute');
-root.style.setProperty('--ul-transform', 'translateY(-150px)');
-root.style.setProperty('--icon-display', 'flex');
-
-setTimeout(
-    () => navLinks.style.transition = 'transform .3s ease-in-out',
-    200
-);
+navLinks.style.transition = 'transform .3s ease-in-out';
 
 function openNav() {
     nav.classList.toggle('open');
